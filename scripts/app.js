d3b878447bbcffc830ce508199fd68ce324e308f
Alex Read <alexread730@gmail.com>
2017-07-11 15:41:20 -0600

    added books to page

--- scripts/app.js
+++ scripts/app.js
@@ -1,33 +1,4 @@
 $(() => {
 
-  const BOOKS_URL = 'http://localhost:3000/api/v1/books';
-
-  function parseJSON(response) {
-    return response.json();
-  }
-
-  function throwError(res) {
-    return new Error("Error")
-  }
-
-
-  makeBooksRequest(BOOKS_URL)
-
-  function makeBooksRequest(url) {
-    const bookRequest = new Request(url, {
-      method: "get",
-      mode: 'cors'
-    })
-    getBooks(bookRequest)
-  };
-
-  function getBooks(request) {
-    fetch(request)
-      .then(parseJSON)
-      .then(response => {
-        console.log(response);
-      })
-      .catch(throwError)
-  }
 
 });
---
+++ scripts/books.js
@@ -0,0 +1,65 @@
+$(() => {
+
+  const BOOKS_URL = 'http://localhost:3000/api/v1/books';
+
+  function parseJSON(response) {
+    return response.json();
+  }
+
+  function throwError(res) {
+    return new Error("Error")
+  }
+
+
+  makeBooksRequest(BOOKS_URL)
+
+  function makeBooksRequest(url) {
+    const bookRequest = new Request(url, {
+      method: "get",
+      mode: 'cors'
+    })
+    getBooks(bookRequest)
+  };
+
+  function getBooks(request) {
+    fetch(request)
+      .then(parseJSON)
+      .then(response => {
+        console.log(response);
+        appendBooks(response);
+      })
+      .catch(throwError)
+  }
+
+  function appendBooks(response) {
+    response.forEach(book => {
+      console.log(book);
+
+
+      $('.books-wrapper').append(`
+        <div class="card-wrapper">
+          <div class="col s12 m7">
+            <div class="card horizontal">
+              <div class="card-image">
+                <img src="${book.cover_url}">
+              </div>
+              <div class="card-stacked">
+                <div class="card-content">
+                  <h3 class="header">${book.title}</h3>
+                  <h5>${book.authors}</h5>
+                  <p>${book.genre}</p>
+                  <p>${book.description}</p>
+                </div>
+                <div class="card-action">
+                  <a class="waves-effect waves-light btn blue">Edit</a>
+                  <a class="waves-effect waves-light btn red">Remove</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        `);
+    });
+  }
+
+});
