19ab4be5e5522633df72852ea57e548ddd231668
Alex Read <alexread730@gmail.com>
2017-07-11 13:20:03 -0600

    working getAllBooks

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,33 @@
-$(()=> {
-  console.log('hi from jquery!');
+$(() => {
+
+  const BOOKS_URL = 'http://localhost:3000/api/v1/books';
+
+  function parseJSON(response) {
+    return response.json();
+  }
+
+  function throwError(res) {
+    return new Error("Error")
+  }
+
+
+  makeBooksRequest(BOOKS_URL)
+
+  function makeBooksRequest(url) {
+    const bookRequest = new Request(url, {
+      method: "get",
+      mode: 'cors'
+    })
+    getBooks(bookRequest)
+  };
+
+  function getBooks(request) {
+    fetch(request)
+      .then(parseJSON)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(throwError)
+  }
+
 });
